3c27d72113a0613e31a91db43d2b330f96c3d035
belogolovm <49549322+belogolovm@users.noreply.github.com>
2021-05-27 21:20:01 +0300

    Delete src/maps-neft.ru directory

--- src/maps-neft.ru/connect.php
+++
@@ -1,9 +0,0 @@
-			<?php    
-			
-			 $dbconn = pg_connect("host=127.0.0.1 dbname=geobd user=geobd1 password=geobd1");
-  if (!$dbconn)
-{
- echo "Нет соединения с базой данных\n";
- exit;
-}
-			?>
